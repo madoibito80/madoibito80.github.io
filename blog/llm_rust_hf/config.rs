f5d6d14b2123882881541085fafb28b4dbedfd59
madoibito80 <rei.sato.phper@gmail.com>
2024-11-23 17:41:55 +0900

    add text

--- blog/llm_rust_hf/config.rs
+++ blog/llm_rust_hf/config.rs
@@ -1,5 +1,7 @@
-// Sourced from https://huggingface.co/meta-llama/Llama-3.2-1B-Instruct/blob/c4219cc9e642e492fd0219283fa3c674804bb8ed/config.json
 pub const DEVICE: candle_core::Device = candle_core::Device::Cpu;
+pub const FORWARD_DTYPE: candle_core::DType = candle_core::DType::F32;
+
+// Sourced from https://huggingface.co/meta-llama/Llama-3.2-1B-Instruct/blob/c4219cc9e642e492fd0219283fa3c674804bb8ed/config.json
 pub const HEAD_DIM: usize = 64;
 pub const HIDDEN_SIZE: usize = 2048;
 pub const MAX_POSITION_EMBEDDINGS: usize = 131072;
@@ -15,4 +17,3 @@ pub mod rope_scaling {
 }
 pub const ROPE_THETA: f32 = 500000.0;
 pub const BOS_TOKEN_ID: u32 = 128000;
-pub const FORWARD_DTYPE: candle_core::DType = candle_core::DType::F32;
